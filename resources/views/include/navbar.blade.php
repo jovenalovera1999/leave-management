10f367c1b153600e0e897f4b818108b4df82cabc
unknown <DevHelloThere>
2024-06-01 01:48:06 +0800

    Sidebar and navbar modified

--- resources/views/include/navbar.blade.php
+++ resources/views/include/navbar.blade.php
@@ -1,12 +1,12 @@
-<nav class="navbar navbar-expand-lg bg-body-tertiary shadow">
+<nav class="navbar navbar-expand-lg bg-body-tertiary shadow sticky-top">
     <div class="container-fluid">
         <ul class="navbar-nav">
             <li class="nav-item dropdown">
-                <a class="nav-link dropdown-toggle text-black" href="#" role="button" data-bs-toggle="dropdown"
+                <a class="nav-link dropdown-toggle text-black" role="button" data-bs-toggle="dropdown"
                     aria-expanded="false">
-                    NAME OF USER
+                    <span class="text-success fs-6 me-2">LOGGED IN AS:</span> THOMAS SHELBY
                 </a>
-                <ul class="dropdown-menu">
+                <ul class="dropdown-menu dropdown-menu-end">
                     <li><a class="dropdown-item" href="#">LOGOUT</a></li>
                 </ul>
             </li>
--- resources/views/include/sidebar.blade.php
+++ resources/views/include/sidebar.blade.php
@@ -6,7 +6,7 @@
     <div class="d-flex flex-column">
         <div class="profile">
             <h1 class="text-light mt-3">LEAVE MANAGEMENT</h1>
-            <img src="{{ asset('img/general/logo.png') }}" alt="" class="img-fluid rounded-circle bg-light">
+            <img src="{{ asset('img/general/Logo.png') }}" alt="" class="img-fluid rounded-circle bg-light" />
         </div>
         <!-- .nav-menu -->
         <nav id="navbar" class="nav-menu navbar">
@@ -18,7 +18,6 @@
                 <li><a href="/departments"><span>DEPARTMENTS</span></a></li>
                 <li><a href="/positions"><span>POSITIONS</span></a></li>
                 <li><a href="#"><span>TYPES OF LEAVE</span></a></li>
-                <li><a href="#"><span>LOGOUT</span></a></li>
             </ul>
         </nav>
     </div>
